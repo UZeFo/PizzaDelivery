36100e04b8c188a66cb38e28a4474299205f8d33
UZeFo <109690971+UZeFo@users.noreply.github.com>
2022-09-19 15:00:19 -0300

    Update.point.js

--- points.js
+++ points.js
@@ -6,8 +6,8 @@ var geojson = {
             "geometry": {
                 "type": "Point",
                 "coordinates": [
-                    4.87408,
-                    52.37617
+                    13.376481, 
+                    52.509663 
                 ]
             },
             "properties": {
@@ -20,8 +20,9 @@ var geojson = {
             "geometry": {
                 "type": "Point",
                 "coordinates": [
-                    4.86615,
-                    52.35100
+                    13.38144,
+                    52.51067
+
                 ]
             },
             "properties": {
@@ -34,14 +35,62 @@ var geojson = {
             "geometry": {
                 "type": "Point",
                 "coordinates": [
-                    4.94307,
-                    52.35294
+                    13.23950406432125,
+                    52.51481788851762 
                 ]
             },
             "properties": {
                 "iconUrl": "img/ic_restaurant_3_map.png",
                 "polygonColor": "#e6f2a2"
+            }var geojson = {
+    "type": "FeatureCollection",
+    "features": [
+        {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    13.33775469738728,
+                    52.50801100146849 
+                ]
+            },
+            "properties": {
+                "iconUrl": "img/ic_restaurant_1_map.png",
+                "polygonColor": "#abc3f2"
             }
+        },var geojson = {
+    "type": "FeatureCollection",
+    "features": [
+        {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    13.277679397382565, 
+                    52.41262284138264 
+                ]
+            },
+            "properties": {
+                "iconUrl": "img/ic_restaurant_1_map.png",
+                "polygonColor": "#abc3f2"
+            }
+        },var geojson = {
+    "type": "FeatureCollection",
+    "features": [
+        {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    13.369491997388057, 
+                    52.52503510404505 
+                ]
+            },
+            "properties": {
+                "iconUrl": "img/ic_restaurant_1_map.png",
+                "polygonColor": "#abc3f2"
+            }
+        },
         }
     ]
 };
